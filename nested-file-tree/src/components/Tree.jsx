08a5144237d46018196dc6b1924b3342aeb50fc2
Anshuman Upadhyay <anshumanupy@gmail.com>
2024-04-28 23:22:12 +0530

    added toggling on a directory node with css and react state

--- nested-file-tree/src/components/Tree.jsx
+++ nested-file-tree/src/components/Tree.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react"
+
 export default function Tree({ data, adder }) {
+    const [show, setShow] = useState(false);
+    function handleClick(e) {
+        e.stopPropagation(); // Remove this and see the counter getting increment by no. parents above it
+        console.log("clicked");
+        setShow(!show);
+    }
     let children = data.children.map((item, index) => {
         return (
             <>
@@ -6,16 +14,20 @@ export default function Tree({ data, adder }) {
                     item.type == "Directory" ? <Tree
                     key={`${item.key.toUpperCase()}_${index}`} 
                     adder={adder + 2}
-                 data={item}></Tree> : <p style={{marginLeft: `${((adder+2) * 2)}rem`}} key={item.key}>{item.name}</p>
+                 data={item}></Tree> : 
+                 <p style={{marginLeft: `${((adder+2) * 2)}rem`}} 
+                 key={item.key}>{item.name}</p>
                 }
                  
             </>
         )
     })
     return (
-       <div className="container">
+       <div className="container" onClick={handleClick}>
         <p style={{marginLeft: `${(adder * 2)}rem`}}>{data.name}</p>
+        <div style={{"display": show ? "none" : "block"}}>
             {children}
+        </div>
        </div>
     )
 }
